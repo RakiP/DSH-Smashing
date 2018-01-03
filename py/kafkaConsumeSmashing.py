da341d03be4ccc62af083351ba1b46c388147204
RakiP <r.partapsing@gmail.com>
2018-01-03 13:17:58 +0100

    introduced ipAddress variable

--- py/kafkaConsumeSmashing.py
+++ py/kafkaConsumeSmashing.py
@@ -3,18 +3,18 @@ from kafka import KafkaConsumer
 import requests
 
 knpTenant = 'smashing'
-ipAddress = '172.22.0.1:9092'
+ipAddress = '172.22.0.1'
 
 def readsendTenant(tenant):
     global knpTenant
-    consumer = KafkaConsumer(knpTenant, group_id='smash-group', bootstrap_servers=[ipAddress])
+    consumer = KafkaConsumer(knpTenant, group_id='smash-group', bootstrap_servers=[ipAddress+':9092'])
     for message in consumer:
         if message.key == '{}'.format(tenant).encode():
             jDct = json.loads(message.value.decode('utf-8'))
             print('Sending new Testresults of {}'.format(tenant))
-            requests.post("http://172.22.0.1:5000/widgets/{}".format(tenant), json=jDct)
+            requests.post("http://{}:5000/widgets/{}".format(ipAddress, tenant), json=jDct)
             # consume earliest available messages, don't commit offsets
-    KafkaConsumer(bootstrap_servers=[ipAddress], auto_offset_reset='earliest', enable_auto_commit=False)
+    KafkaConsumer(bootstrap_servers=[ipAddress+':9092'], auto_offset_reset='earliest', enable_auto_commit=False)
 
 def main():
     ############VIALIS##############
