6762ec300f6b4f88e3dd4df23e029aa5d3bcf904
Partapsing, Rakesh <rakesh.partapsing@kpn.com>
2018-01-04 19:46:00 +0100

    no more abs path in changeme

--- changeme.sh
+++ changeme.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-: "${pathToListener:="$HOME/Documents/projects/DSHTesting/DSH-Smashing/py"}"
+: "${pathToListener:="$PWD/py"}"
 IP=`ifconfig | grep inet | grep -v inet6 | head -1 | awk '{print $2}' | cut -d':' -f2`
 sed -i -e "s/IPADDRESS/$IP/g" "py/kafkaConsumeSmashing.py"
 sed -i -e "s/IPADDRESS/$IP/g" "makefile"
